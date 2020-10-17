7438a118542a77e51222231394ba9c64235042b8
Schaeferjo <mail@joschaefer.com>
2020-10-17 16:53:32 +0200

    feat: add blog post listing

---
+++ pages/blog/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <ul>
+      <li v-for="post of posts" :key="post.slug">
+        <NuxtLink :to="'/blog/' + post.slug">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content('blog').fetch()
+
+    return {
+      posts,
+    }
+  },
+}
+</script>
