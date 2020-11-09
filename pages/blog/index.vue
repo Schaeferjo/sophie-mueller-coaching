38c93476f92ef50cb7367ef99177921debbc6d98
Schaeferjo <mail@joschaefer.com>
2020-11-09 13:11:10 +0100

    fix: lint

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6"> 
-        <h1>Blog</h1>
-    <ul>
-      <li v-for="post of posts" :key="post.slug">
-        <NuxtLink :to="'/blog/' + post.slug">{{ post.title }}</NuxtLink>
-      </li>
-    </ul>
+    <v-col cols="12" sm="8" md="6">
+      <h1>Blog</h1>
+      <ul>
+        <li v-for="post of posts" :key="post.slug">
+          <NuxtLink :to="'/blog/' + post.slug">{{ post.title }}</NuxtLink>
+        </li>
+      </ul>
     </v-col>
   </v-row>
 </template>
--- pages/test.vue
+++ pages/test.vue
@@ -1,47 +1,43 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-     
       <h1>Test</h1>
 
       <ul class="py-12">
-        <li v-for='(buzzword, index) in buzzwords' :key="buzzword" :class="{ active: index === activePosition }">
-          {{ buzzword }} 
+        <li
+          v-for="(buzzword, index) in buzzwords"
+          :key="buzzword"
+          :class="{ active: index === activePosition }"
+        >
+          {{ buzzword }}
         </li>
       </ul>
-
     </v-col>
   </v-row>
 </template>
-
-
 
 <script>
 export default {
   data() {
     return {
       activePosition: 0,
-      buzzwords: [ 'Eins', 'Zwei', 'Drei und noch was' ]
+      buzzwords: ['Eins', 'Zwei', 'Drei und noch was'],
     }
   },
-  mounted(){ 
+  mounted() {
     this.changeActivePosition()
   },
-  methods:{
-    changeActivePosition(){
+  methods: {
+    changeActivePosition() {
       this.activePosition = (this.activePosition + 1) % this.buzzwords.length
       setTimeout(this.changeActivePosition, 1000)
-
-    }
-  }
-  
+    },
+  },
 }
 </script>
 
 <style>
-
-.active{
-  font-weight:bold;
+.active {
+  font-weight: bold;
 }
-
 </style>
