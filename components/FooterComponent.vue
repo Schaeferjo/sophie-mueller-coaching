6b82913afe97fc36e5d0d3f65fd3e7d5fdb4cc93
Schaeferjo <mail@joschaefer.com>
2020-11-09 13:19:57 +0100

    fix: lint

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -1,16 +1,15 @@
 <template>
-  <v-container >
+  <v-container>
     <v-row>
-    <v-col cols="12" xs="12" sm="12" md="12" lg="12" xl="12">
-          Footer {{ new Date().getFullYear() }}
+      <v-col cols="12" xs="12" sm="12" md="12" lg="12" xl="12">
+        Footer {{ new Date().getFullYear() }}
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-
 <style lang="scss" scoped>
-  div {
-    color:red;
-  }
+div {
+  color: red;
+}
 </style>
